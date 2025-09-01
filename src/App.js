0982edd966d4a30b8a1428d585f8cae1ada5d41c
baribo-yama <kenta6flat@icloud.com>
2025-09-02 07:44:19 +0900

    今の気持ちを書いてみた

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>ポートフォリオ</h1>
-      <p>私はサイコパスです</p>
-      <p>スーキャッスルは糖尿病</p>
+      <p>寿司食べたい</p>
+      <p>寿司打得意</p>
       <img src="/apple.jpg" alt="apple" width="200" />
     </div>
   );
