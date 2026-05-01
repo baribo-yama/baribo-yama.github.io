4795b885d3a66fd2b356a02a74c7bfbc32c49389
baribo-yama <kenta6flat@icloud.com>
2026-05-01 22:24:08 +0900

    typescriptに対応させた

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <h1>ポートフォリオ</h1>
+    <div className="flex flex-col items-center min-h-screen text-center">
+      <h1 className="text-3xl font-bold underline">ポートフォリオ</h1>
       <p>寿司食べたい</p>
       <p>寿司打得意</p>
       <img src="/apple.jpg" alt="apple" width="200" />
